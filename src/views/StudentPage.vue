76e504010422682fe620869b7fdf8cb0c6447310
a5650057 <a5650057@gmail.com>
2024-02-05 17:45:59 +0800

    init

--- src/views/StudentPage.vue
+++ src/views/StudentPage.vue
@@ -1,13 +1,8 @@
-﻿
-
-
-
-<template>
+﻿<template>
     <button @click = "return_home">主目錄 </button>
     <div class="video-container">
       <div class="video-stream" v-for="n in 5" :key="n">
         <video controls>
-
           <source src="http://www.w3school.com.cn/i/movie.mp4" type="video/mp4">
           Your browser does not support the video tag.
         </video>
