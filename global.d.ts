2b9dad4a45b6a520ed9f5c8565894e475181b8b6
a5650057 <a5650057@gmail.com>
2024-02-29 20:55:09 +0800

    2024-2-29

---
+++ global.d.ts
@@ -0,0 +1,11 @@
+﻿// global.d.ts
+export {};
+
+declare global {
+  interface Window {
+    electronAPI: {
+      on: (channel: string, func: (...args: any[]) => void) => void;
+      // 定義其他需要的方法
+    };
+  }
+}
